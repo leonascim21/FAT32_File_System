8f63faebc987ed575530979a0929233970159e51
leonascim <leonascim21@gmail.com>
2024-12-05 20:30:18 -0500

    implement rmdir command

--- main.c
+++ main.c
@@ -993,6 +993,128 @@ void write_file(char *filename, char *string) {
     }
 }
 
+void rmdir(char *dirname) {
+    uint32_t current_cluster = current_directory_cluster;
+    uint32_t dir_cluster = 0;
+    uint32_t dir_entry_offset = 0;
+    DirectoryEntry dir;
+    int found = 0;
+
+    while (1) {
+        uint32_t first_sector = cluster_to_sector(current_cluster);
+        uint32_t byte_offset = first_sector * bpb.BytsPerSec;
+        fseek(fp, byte_offset, SEEK_SET);
+
+        while (fread(&dir, sizeof(DirectoryEntry), 1, fp) == 1) {
+            if (dir.DIR_Name[0] == 0x00) {
+                break;
+            }
+            if (dir.DIR_Name[0] == 0xE5) {
+                continue;
+            }
+            if ((dir.DIR_Attr & 0x0F) == 0x0F) {
+                continue;
+            }
+
+            char name[12];
+            memcpy(name, dir.DIR_Name, 11);
+            name[11] = '\0';
+            for (int i = 10; i >= 0; i--) {
+                if (name[i] == ' ') {
+                    name[i] = '\0';
+                } else {
+                    break;
+                }
+            }
+
+            if (strcmp(name, dirname) == 0) {
+                if (!(dir.DIR_Attr & ATTR_DIRECTORY)) {
+                    printf("not a directory.\n");
+                    return;
+                }
+                if (strcmp(name, ".") == 0 || strcmp(name, "..") == 0) {
+                    printf("Cannot remove special directories '.' or '..'.\n");
+                    return;
+                }
+
+                found = 1;
+                dir_entry_offset = ftell(fp) - sizeof(DirectoryEntry);
+                dir_cluster = ((uint32_t)dir.DIR_FstClusHI << 16) | dir.DIR_FstClusLO;
+                break;
+            }
+        }
+
+        if (found) {
+            break;
+        }
+
+        uint32_t fat_offset = current_cluster * 4;
+        uint32_t fat_sector = bpb.RsvdSecCnt + (fat_offset / bpb.BytsPerSec);
+        uint32_t fat_offset_within_sector = fat_offset % bpb.BytsPerSec;
+
+        fseek(fp, fat_sector * bpb.BytsPerSec + fat_offset_within_sector, SEEK_SET);
+        fread(&current_cluster, sizeof(uint32_t), 1, fp);
+        current_cluster &= 0x0FFFFFFF;
+
+        if (current_cluster >= EOC) {
+            break;
+        }
+    }
+
+    if (!found) {
+        printf("Directory not found.\n");
+        return;
+    }
+
+    uint32_t first_sector = cluster_to_sector(dir_cluster);
+    uint32_t byte_offset = first_sector * bpb.BytsPerSec;
+    fseek(fp, byte_offset, SEEK_SET);
+
+    while (fread(&dir, sizeof(DirectoryEntry), 1, fp) == 1) {
+        if (dir.DIR_Name[0] == 0x00) {
+            break;
+        }
+        if (dir.DIR_Name[0] != 0xE5 && !(dir.DIR_Attr & 0x0F)) {
+            char name[12];
+            memcpy(name, dir.DIR_Name, 11);
+            name[11] = '\0';
+            for (int i = 10; i >= 0; i--) {
+                if (name[i] == ' ') {
+                    name[i] = '\0';
+                } else {
+                    break;
+                }
+            }
+            if (strcmp(name, ".") != 0 && strcmp(name, "..") != 0) {
+                printf("Directory is not empty.\n");
+                return;
+            }
+        }
+    }
+
+    uint32_t cluster = dir_cluster;
+    while (cluster < EOC && cluster != 0x0000000) {
+        uint32_t fat_offset = cluster * 4;
+        uint32_t fat_sector = bpb.RsvdSecCnt + (fat_offset / bpb.BytsPerSec);
+        uint32_t fat_offset_within_sector = fat_offset % bpb.BytsPerSec;
+
+        fseek(fp, fat_sector * bpb.BytsPerSec + fat_offset_within_sector, SEEK_SET);
+        uint32_t next_cluster;
+        fread(&next_cluster, sizeof(uint32_t), 1, fp);
+        next_cluster &= 0x0FFFFFFF;
+
+        uint32_t free_value = 0x00000000;
+        fseek(fp, fat_sector * bpb.BytsPerSec + fat_offset_within_sector, SEEK_SET);
+        fwrite(&free_value, sizeof(uint32_t), 1, fp);
+
+        cluster = next_cluster;
+    }
+
+    fseek(fp, dir_entry_offset, SEEK_SET);
+    uint8_t deleted_marker = 0xE5;
+    fwrite(&deleted_marker, sizeof(uint8_t), 1, fp);
+}
+
 
 int main(int argc, char *argv[]) {
     //error checking for if more than 1 arg is provided when running program
@@ -1055,6 +1177,8 @@ int main(int argc, char *argv[]) {
             rename_file(tokens[1], tokens[2]);
         } else if (strcmp(tokens[0], "write") == 0 && token_count > 2) {
             write_file(tokens[1], tokens[2]);
+        } else if (strcmp(tokens[0], "rmdir") == 0 && token_count > 1) {
+            rmdir(tokens[1]);
         } else {
             printf("Unknown command\n");
         }
